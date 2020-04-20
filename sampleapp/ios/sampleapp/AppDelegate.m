dab8a14a69a9308e534225f32b59a10922ebcd1e
Olivier Favre <olivier@wonderpush.com>
2020-04-20 17:20:39 +0200

    Add debug logs on iOS sample app

--- sampleapp/ios/sampleapp/AppDelegate.m
+++ sampleapp/ios/sampleapp/AppDelegate.m
@@ -14,6 +14,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [WonderPush setLogging:YES];
     [WonderPush setClientId:@"fd49eef17401e6b2916e9101fa48c9c2f15ec330" secret:@"fd9b63c4c77c9a66d00aa64e5aed8d25e8ccb510e96baff8b08a8a980777e1c6"];
     [WonderPush setupDelegateForApplication:application];
     [WonderPush setupDelegateForUserNotificationCenter];
