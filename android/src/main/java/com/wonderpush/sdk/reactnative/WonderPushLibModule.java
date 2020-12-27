5a91ccd142a4a5db0947852bb16bd0ba3b5ec93b
Olivier Favre <olivier@wonderpush.com>
2020-12-27 16:17:51 +0100

    2.0.1

--- android/src/main/java/com/wonderpush/sdk/reactnative/WonderPushLibModule.java
+++ android/src/main/java/com/wonderpush/sdk/reactnative/WonderPushLibModule.java
@@ -27,7 +27,7 @@ public class WonderPushLibModule extends ReactContextBaseJavaModule {
     public WonderPushLibModule(final ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        WonderPush.setIntegrator("react-native-wonderpush-2.0.0");
+        WonderPush.setIntegrator("react-native-wonderpush-2.0.1");
 
         LocalBroadcastManager.getInstance(reactContext).registerReceiver(new BroadcastReceiver() {
             @Override
