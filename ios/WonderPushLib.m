4fd3db3892394c85412314b3bfbc2adedf313df1
Stéphane JAIS <stephane@wonderpush.com>
2021-04-19 15:22:13 +0200

    2.0.3

--- ios/WonderPushLib.m
+++ ios/WonderPushLib.m
@@ -14,7 +14,7 @@
 }
 
 + (void)initialize {
-    [WonderPush setIntegrator:@"react-native-wonderpush-2.0.2"];
+    [WonderPush setIntegrator:@"react-native-wonderpush-2.0.3"];
     __block WonderPushLib *blocksafeSelf = self;
     
     [[NSNotificationCenter defaultCenter] addObserverForName:WP_NOTIFICATION_OPENED_BROADCAST object:nil queue:nil usingBlock:^(NSNotification *note) {
