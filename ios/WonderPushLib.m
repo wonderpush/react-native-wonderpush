15ad11fa1de1acb0c68b9926e49f7234c6998572
Olivier Favre <olivier@wonderpush.com>
2022-01-04 15:58:22 +0100

    2.0.12

--- ios/WonderPushLib.m
+++ ios/WonderPushLib.m
@@ -14,7 +14,7 @@
 }
 
 + (void)initialize {
-    [WonderPush setIntegrator:@"react-native-wonderpush-2.0.11"];
+    [WonderPush setIntegrator:@"react-native-wonderpush-2.0.12"];
 }
 
 - (instancetype) init {
