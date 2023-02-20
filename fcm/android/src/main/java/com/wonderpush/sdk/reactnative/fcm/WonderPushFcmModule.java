a5dba602adb9f18c7bc6ebfe0688c4a9ea5ede0c
Olivier Favre <olivier@wonderpush.com>
2023-02-20 11:31:53 +0100

    Remove Kotlin from the FCM and HCM modules to fix compilation

---
+++ fcm/android/src/main/java/com/wonderpush/sdk/reactnative/fcm/WonderPushFcmModule.java
@@ -0,0 +1,12 @@
+package com.wonderpush.sdk.reactnative.fcm;
+
+import com.facebook.react.bridge.*;
+
+public class WonderPushFcmModule extends ReactContextBaseJavaModule {
+
+    @Override
+    public String getName() {
+        return "WonderPushFcm";
+    }
+
+}
---
+++ hcm/android/src/main/java/com/wonderpush/sdk/reactnative/hcm/WonderPushHcmModule.java
@@ -0,0 +1,12 @@
+package com.wonderpush.sdk.reactnative.hcm;
+
+import com.facebook.react.bridge.*;
+
+public class WonderPushHcmModule extends ReactContextBaseJavaModule {
+
+    @Override
+    public String getName() {
+        return "WonderPushHcm";
+    }
+
+}
