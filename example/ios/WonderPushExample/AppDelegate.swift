3eb4389e20d3d385ebee5251ae2a8cb96dc81302
Olivier Favre <olivier@wonderpush.com>
2025-09-18 15:03:06 +0200

    feat(example): initialize WonderPush SDK from native code

--- example/ios/WonderPushExample/AppDelegate.swift
+++ example/ios/WonderPushExample/AppDelegate.swift
@@ -10,6 +10,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
 
+  func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    WonderPush.setLogging(true)
+    WonderPush.setRequiresUserConsent(false)
+    WonderPush.setClientId("7524c8a317c1794c0b23895dce3a3314d6a24105", secret: "b43a2d0fbdb54d24332b4d70736954eab5d24d29012b18ef6d214ff0f51e7901")
+    WonderPush.setupDelegate(for: application)
+    WonderPush.setupDelegateForUserNotificationCenter()
+    return true
+  }
+
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
