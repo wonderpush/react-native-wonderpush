19283f21d8f2cff46c0412391004817b701cc314
Olivier Favre <olivier@wonderpush.com>
2025-10-01 17:00:52 +0200

    feat(example): notification service extension

---
+++ example/ios/WonderPushNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  WonderPushNotificationServiceExtension
+//
+//  Created by Olivier Favre on 30/09/2025.
+//
+
+#import <WonderPushExtension/WonderPushExtension.h>
+
+@interface NotificationService : WPNotificationServiceExtension
+
+@end
